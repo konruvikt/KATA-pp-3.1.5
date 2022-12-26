0bd2a53bfac4af20b01beb3b0fb99cf805258568
konruvikt <114674272+konruvikt@users.noreply.github.com>
2022-12-26 17:36:55 +0300

    failed to show user-list page with js

--- src/main/java/org/konruvikt/kata_pp_313/controllers/RESTAdminController.java
+++ src/main/java/org/konruvikt/kata_pp_313/controllers/RESTAdminController.java
@@ -2,25 +2,26 @@ package org.konruvikt.kata_pp_313.controllers;
 
 import org.konruvikt.kata_pp_313.models.User;
 import org.konruvikt.kata_pp_313.services.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
-public class AdminController {
+public class RESTAdminController {
 
     private final UserService userService;
 
-    public AdminController(UserService userService) {
+    public RESTAdminController(UserService userService) {
         this.userService = userService;
     }
 
 
     @GetMapping
-    public List<User> findAll(){
+    public ResponseEntity<List<User>> findAll(){
         List<User> allUsers = userService.findAll();
-        return allUsers;
+        return ResponseEntity.ok(allUsers);
     }
 
     @GetMapping("/{id}")
---
+++ src/main/resources/static/TableOfAllUsers.js
@@ -0,0 +1,31 @@
+$(async function() {
+    await allUsers();
+});
+const table = $('#usersTable');
+async function allUsers() {
+    table.empty()
+    fetch("http://localhost:8081/admin")
+        .then(res => res.json())
+        .then(data => {
+            data.forEach(user => {
+                let usersTable = `$(
+                        <tr>
+                            <td>${user.id}</td>
+                            <td>${user.name}</td>
+                            <td>${user.lastName}</td>
+                            <td>${user.age}</td>   
+                            <td>${user.login}</td>
+                            <td>${user.roles}</td>
+                            <td>
+                                <button type="button" class="btn btn-warning" data-toggle="modal" id="buttonEdit"
+                                data-action="edit" data-id="${user.id}" data-target="#edit">Edit</button>
+                            </td>
+                            <td>
+                                <button type="button" class="btn btn-danger" data-toggle="modal" id="buttonDelete"
+                                data-action="delete" data-id="${user.id}" data-target="#delete">Delete</button>
+                            </td>
+                        </tr>)`;
+                table.append(usersTable);
+            })
+        })
+}
