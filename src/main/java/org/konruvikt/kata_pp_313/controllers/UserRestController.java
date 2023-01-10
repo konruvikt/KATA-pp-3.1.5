88e21e95e8eb3c08f7a1a00e342ca13e7817215c
konruvikt <114674272+konruvikt@users.noreply.github.com>
2023-01-10 16:57:03 +0300

    AuthenticationPrincipal added

--- src/main/java/org/konruvikt/kata_pp_313/controllers/UserRestController.java
+++ src/main/java/org/konruvikt/kata_pp_313/controllers/UserRestController.java
@@ -2,7 +2,7 @@ package org.konruvikt.kata_pp_313.controllers;
 
 import org.konruvikt.kata_pp_313.models.User;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserRestController {
 
     @GetMapping("/userAuth")
-    public ResponseEntity<User> showAuthUser(Authentication authentication) {
-        User user = (User) authentication.getPrincipal();
+    public ResponseEntity<User> showAuthUser(@AuthenticationPrincipal User user) {
         return ResponseEntity.ok(user);
     }
 }
